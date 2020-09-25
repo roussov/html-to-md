235540fac7167a3f3b288b2ae839c56f6ee71c36
vincent.rs <vincent.rousseau.pro@gmail.com>
2020-09-25 09:54:24 +0200

    update

--- setup.sh
+++ setup.sh
@@ -4,13 +4,12 @@
 function dependencies(){
     echo "Dépendances du programme"
     echo "==========================="
-    clear
     while true; do
     read -p "Avez-vous déjà installer les dépendances python [essentiels au script] ?: [Y,y / N,n]" yn
     case $yn in
         [Yy]* ) sendmail_config ;;
         [Nn]* ) pip3 install -r requirements.txt 
-        
+
         * ) echo "Répondez par oui ou par non";;
     esac
 
@@ -23,7 +22,7 @@ done
 function sendmail_config(){
     echo "Configuration du module sendmail"
     echo "============================="
-    clear
+
      while true; do
     read -p "Avez-vous déjà installé le module sendmail [essentiel au serveur] ?: [Y,y / N,n]" yn
     case $yn in
@@ -33,19 +32,15 @@ function sendmail_config(){
          * ) echo "Répondez par oui ou par non";;
     esac
 done
-clear
+
 
 }
 
 function start-service(){
     sudo sendmailconfig
-<<<<<<< HEAD
-    sudo service apache2 restart 
-=======
     sudo service apache2 restart
-    python news.py
-    ./email.sh   
->>>>>>> 2f45971b48c7e8b66dcdad680feb909b6866e6d3
+    ./start.sh
+
 }
 
 

74145bc8a923fcc8b0b905e2b0f757da9840be3e
vincent.rs <vincent.rousseau.pro@gmail.com>
2020-09-25 10:09:46 +0200

    update

--- email.sh
+++ email.sh
@@ -1,14 +1,20 @@
 #!/bin/sh
 #Weather for Jacky!
 #
-subject="-Email Météo-"
+
+
 DAY=$(date +%d)
 MONTH=$(date +%m)
 YEAR=$(date +%y)
+news_today=news-$DAY-$MONTH-$YEAR.txt
+
+mv news.txt $news_today
+
+
 
 read -p "Entrez l'adresse email du destinataire: " TO
 read -p "Entrez le message: " subject
-sendmail -t $TO -m $subject -a weather-$DAY-$MONTH-$YEAR.txt
-time sleep
+sendmail -t $TO -m $subject -a $news_today
+
 
 

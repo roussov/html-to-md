29ee1d80a8ef342de7876c7f4106192c5ac0b10e
Vincent Rousseau <12572924+roussov@users.noreply.github.com>
2025-02-25 15:27:07 +0100

    Add files via upload

--- start.sh
+++ start.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 #start.sh
 
+
 function start-test(){
     echo "Bienvenue [test des modules]"
     echo "==========================="
     while true; do
     read -p "Avez-vous déjà configuré le serveur ?: [Y,y / N,n]" yn
     case $yn in
-        [Yy]* ) news-txt ;;
+        [Yy]* ) news-txt || exit 0 ;;
         [Nn]* ) ./setup.sh ;;
         * ) echo "Répondez par oui ou par non";;
     esac
 done
-} 
+}
 
 
 
@@ -24,6 +25,5 @@ function news-txt(){
 function main(){
     start-test
     news-txt
-    
 }
 main
